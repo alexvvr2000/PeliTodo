c05e7aff49dc255cec81877f5c80305b044bee3d
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-09-28 17:22:12 -0600

    agregadas tablas de criticas de peliculas

--- esquema/1 esquema.sql
+++ esquema/1 esquema.sql
@@ -154,3 +154,15 @@ create table pelicula_lista(
     constraint fk_lista_pelicula foreign key(id_lista) references lista_usuario(id_lista),
     constraint pk_pelicula_lista primary key(id_lista, id_pelicula)
 );
+
+create table critica(
+    id_usuario int not null,
+    id_pelicula int not null,
+    corazon bool null,
+    estrellas decimal(2,1) not null,
+    descripcion tinytext null,
+    fecha_publicacion datetime not null,
+    constraint fk_usuario_critica foreign key(id_usuario) references usuario(id_usuario),
+    constraint fk_pelicula_critica foreign key(id_pelicula) references pelicula(id_pelicula),
+    constraint pk_critica primary key(id_pelicula, id_usuario)
+);
