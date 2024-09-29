e003f8948a86d9081c13b93e421f2d3ce2fe5497
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-09-29 10:52:45 -0600

    ahora enumeracion de esquema usa numeracion XX_nombre_.sql

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM mysql:8.0.39-debian
 
-COPY ./esquema/1 esquema.sql /docker-entrypoint-initdb.d/
+COPY ./esquema/01_esquema.sql /docker-entrypoint-initdb.d/
--- esquema/01_esquema.sql
+++ esquema/01_esquema.sql
@@ -1,25 +1,28 @@
 create table lenguaje(
-    id_lenguaje char(2) primary key,
-    nombre varchar(30) not null
+    id_lenguaje char(2) not null,
+    nombre varchar(30) not null,
+    constraint pk_lenguaje primary key(id_lenguaje)
 );
 
 create table pais(
-    id_pais char(2) primary key,
+    id_pais char(2) not null,
     codigo_alfa_3 char(3) not null,
-    descripcion varchar(30) not null
+    descripcion varchar(30) not null,
+    constraint pk_pais primary key(id_pais)
 );
 
 create table usuario(
-    id_usuario int primary key auto_increment,
+    id_usuario int not null auto_increment,
     correo_electronico varchar(254) not null,
     nombre_usuario varchar(50) not null,
     salt_clave char(64) not null,
     clave_acceso char(64) not null,
     fecha_creacion datetime not null,
-    fecha_baja datetime not null,
+    fecha_baja datetime null,
     id_pais char(2) not null,
-    constraint fk_pais_usuario foreign key(id_pais) references pais(id_pais)
-)
+    constraint pk_usuario primary key(id_usuario),
+    constraint fk_pais_usuario foreign key (id_pais) references pais(id_pais)
+);
 
 create table plataforma_cuenta_externa(
     id_plataforma int primary key auto_increment,
@@ -29,12 +32,12 @@ create table plataforma_cuenta_externa(
 );
 
 create table cuenta_externa_usuario(
-    id_usuario int,
-    id_plataforma int,
+    id_usuario int not null,
+    id_plataforma int not null,
     usuario_externo varchar(100) not null,
     constraint fk_usuario_cuenta foreign key(id_usuario) references usuario(id_usuario),
     constraint fk_plataforma_usuario foreign key(id_plataforma) references plataforma_cuenta_externa(id_plataforma),
-    constraint pk_cuenta_usuario foreign key(id_usuario, id_plataforma)
+    constraint pk_cuenta_usuario primary key(id_usuario, id_plataforma)
 );
 
 create table estudio(
