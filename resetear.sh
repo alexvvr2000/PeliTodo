f9f55255a7dd2cb5aaea01f4532b9ebbf71b385c
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-11-11 14:55:08 -0600

    agregado borrar inserts al script de resetear

--- resetear.sh
+++ resetear.sh
@@ -3,5 +3,6 @@
 # Esto vuelve a recrear la base de datos desde 0 en caso de querer cambiar el esquema
 
 sudo rm -rf ./mysql_data/* \
+    && rm -rf ./esquema/inserts/* \
     && docker compose build \
     && docker compose up
