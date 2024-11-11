2ad6cb6f2e1ad0f38496fbbc84ce3074ce02802d
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-11-10 19:12:21 -0600

    agregado json wizard de archivo peliculas

--- esquema/script/creacion_insert.py
+++ esquema/script/creacion_insert.py
@@ -1,11 +1,22 @@
+from dataclasses import dataclass
 from pathlib import Path
+
+from dataclass_wizard import JSONWizard
 
 
 def get_docker_secret(secreto: str, default: str = "") -> str:
-    ubicacion: Path = Path(f"/run/secrets/{secreto}")
+    ubicacion = Path(f"/run/secrets/{secreto}")
     if not ubicacion.exists():
         return default
     valor_secreto: str = ""
     with open(ubicacion, "r") as archivo_secreto:
         valor_secreto = archivo_secreto.read().rstrip("\n")
     return valor_secreto
+
+
+ARCHIVO_DATOS = Path("../datos/peliculas.json")
+
+
+@dataclass
+class ArchivoPeliculas(JSONWizard):
+    peliculas: list[str]
