8c29176af44f85bca06630a10905a6d7150d75cf
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-11-11 22:31:25 -0600

    cambiado limite de descripciones de formatos iso de esquema

--- esquema/tablas/01_localizacion.sql
+++ esquema/tablas/01_localizacion.sql
@@ -1,12 +1,12 @@
 create table lenguaje(
     id_lenguaje char(2) not null,
-    nombre varchar(30) not null,
+    nombre varchar(50) not null,
     constraint pk_lenguaje primary key(id_lenguaje)
 );
 
 create table pais(
     id_pais char(2) not null,
     codigo_alfa_3 char(3) not null,
-    descripcion varchar(30) not null,
+    descripcion varchar(50) not null,
     constraint pk_pais primary key(id_pais)
 );
